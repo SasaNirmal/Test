0a3d1979984f3cecdffa487db7dfb0fb1872a384
Sasanka Nirmal <sasanpathirage@gmail.com>
2025-08-22 01:23:41 +0530

    create class and creat object
    Signed-off-by: Sasanka Nirmal <sasanpathirage@gmail.com>

---
+++ rest/src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        addition object = new addition();// create object
+        object.add(5, 10);
+        object.sub(5, 10);
+
+    }
+}
---
+++ rest/src/addition.java
@@ -0,0 +1,12 @@
+public class addition {
+    public  void add(int a,int b){
+      int add = a+b;
+      System.out.println("The sum of " + a + " and " + b + " is: " + add);
+    }
+
+    public void sub(int a, int b) {
+        int sub = a - b;
+        System.out.println("The difference of " + a + " and " + b + " is: " + sub);
+
+    }
+}
